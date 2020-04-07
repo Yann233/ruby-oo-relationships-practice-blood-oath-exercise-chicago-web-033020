fb9540a23a873cd95a97a6e16050f332fa865fce
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-07 11:02:25 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -6,12 +6,10 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-cult1 = Cult.new("cult1", "new york", 2019, 'live fast, die young')
-cult2 = Cult.new("cult2", "los angeles", 2019, 'learn love code')
-cult3 = Cult.new("cult3", "chicago", 2019, 'michael jordan forever')
-cult4 = Cult.new("cult4", "new york", 2019, 'yeet')
-cult5 = Cult.new("cult5", "chicago", 2019, 'new york sucks')
-cult6 = Cult.new("cult6", "chicago", 2019, 'chicago forever')
+cult1 = Cult.new("cult1", "new york", 2010, 'live fast, die young')
+cult2 = Cult.new("cult2", "los angeles", 2011, 'learn love code')
+cult3 = Cult.new("cult3", "chicago", 2012, 'michael jordan forever')
+
 
 
 
