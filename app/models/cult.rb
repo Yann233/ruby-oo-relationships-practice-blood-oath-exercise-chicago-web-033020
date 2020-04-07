42f5c7320167e9c2bb1fd6a95fa85f507c0b0cdb
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-07 11:03:22 +0000

    Automatically backed up by Learn

--- app/models/cult.rb
+++ app/models/cult.rb
@@ -6,7 +6,7 @@ attr_accessor :name,:location,:founding_year,:slogan
     @founding_year = founding_year
     @slogan = slogan
     @followers=[]
-
+end
 #takes in an argument of a Follower instance and adds them to this cult's list of followers
 def recruit_follower(follower)
   @follower
